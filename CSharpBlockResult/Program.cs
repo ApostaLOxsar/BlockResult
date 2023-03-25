ffe9c69d7e0cde5fd2f7975ada4e79abb5d9bc99
Stepan <Stepanmaslov@gmail.com>
2023-03-25 22:54:10 +0300

    Corect project

--- CSharpBlockResult/Program.cs
+++ CSharpBlockResult/Program.cs
@@ -1,17 +1,17 @@
-﻿//
+﻿Console.Clear();
 
-Console.WriteLine("Введите массив строк, разделителем строк в нашем случае выступает (,) Как пример [hello, 2, world, :-)] ");
+Console.WriteLine("Введите массив строк, разделителем строк в нашем случае выступает (,) Как пример [\x0022hello\x0022, \x00222\x0022, \x0022world\x0022, \x0022:-)\x0022]  ");
 string inputString = Console.ReadLine();
-string[] newMassFromInputString = inputString.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+string[] newMassFromInputString = inputString.Trim(new char[] { '[', ']' })
+                                            .Replace("\x0022", "")
+                                            .Split(",", StringSplitOptions.RemoveEmptyEntries);
 
 string result = "";
 
 for (int i = 0; i < newMassFromInputString.Length; i++)
 {
-    if (newMassFromInputString[i].Length <= 3)  result += newMassFromInputString[i] + ",";
-    
+    if (newMassFromInputString[i].Length <= 3) result += newMassFromInputString[i] + ",";
 }
 
-
 string[] resultMas = result.Split(",", StringSplitOptions.RemoveEmptyEntries);
-Console.WriteLine("[" + string.Join(", ", resultMas) + "]");
+Console.WriteLine("\n[" + string.Join(", ", resultMas) + "]");
