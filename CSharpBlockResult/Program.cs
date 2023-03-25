b4d60f4e43e71438b7a14c1ea0620f72f1e09221
Stepan <Stepanmaslov@gmail.com>
2023-03-25 22:18:38 +0300

    Written project in c#

--- CSharpBlockResult/Program.cs
+++ CSharpBlockResult/Program.cs
@@ -1,4 +1,17 @@
 ﻿//
 
-string[] inputString = {"hello", "2", "world", ":-)"};
-Console.WriteLine(string.Join("\t", inputString));
+Console.WriteLine("Введите массив строк, разделителем строк в нашем случае выступает (,) Как пример [hello, 2, world, :-)] ");
+string inputString = Console.ReadLine();
+string[] newMassFromInputString = inputString.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+string result = "";
+
+for (int i = 0; i < newMassFromInputString.Length; i++)
+{
+    if (newMassFromInputString[i].Length <= 3)  result += newMassFromInputString[i] + ",";
+    
+}
+
+
+string[] resultMas = result.Split(",", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine("[" + string.Join(", ", resultMas) + "]");
